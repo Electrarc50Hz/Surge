ce1985df26a1ed30c34c1d3f9378f334bf3df166
Electrarc50Hz <electrarc@outlook.com>
2024-07-03 18:33:56 +0800

    更新 FimoProCrack.js

--- Scripts/FimoProCrack.js
+++ Scripts/FimoProCrack.js
@@ -1,3 +1,3 @@
-# FimoProCrack by @ddgksf2013
+/* FimoProCrack by @ddgksf2013 */
 
 var obj=JSON.parse($response.body);obj.subscribe={valid:!0,forever:1,endTime:4092599349},$done({body:JSON.stringify(obj)});
